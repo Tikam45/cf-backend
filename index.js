33e4196710d2c58e239406e7f0a171cb82ee74ac
Tikam45 <tikamgupta05122004@gmail.com>
2024-12-25 18:35:42 +0530

    url change

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended: false}));
 
 
 app.use(cors({
-    origin: 'https://cf-frontend-sigma.vercel.app/'
+    origin: 'https://cf-frontend-sigma.vercel.app'
 }));
 
 
